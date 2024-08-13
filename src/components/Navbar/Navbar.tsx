fa5eb49861f3993db0276d08d4969a2efd66468b
P A Chathura Madhava <chathuratechdev@gmail.com>
2024-08-13 12:17:05 +0530

    Apply Dark Mode

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -1,3 +1,5 @@
+"use client";
+
 import Image from "next/image";
 import companyLogo from "@/assets/company-logo.png";
 import {
@@ -18,8 +20,13 @@ import {
   DropdownMenuTrigger,
 } from "../ui/dropdown-menu";
 import Link from "next/link";
+import { Button } from "../ui/button";
+import { Moon, Sun } from "lucide-react";
+import { useTheme } from "next-themes";
 
 function Navbar() {
+  const { setTheme } = useTheme()
+
   return (
     <div>
       <div className="flex items-center py-2 justify-between pr-4">
@@ -53,7 +60,7 @@ function Navbar() {
             </NavigationMenuList>
           </NavigationMenu>
         </div>
-        <div className="flex gap-4">
+        <div className="flex gap-4 items-center">
           <div>Cart</div>
           <DropdownMenu>
             <DropdownMenuTrigger>Open</DropdownMenuTrigger>
@@ -64,6 +71,26 @@ function Navbar() {
               <DropdownMenuItem>Billing</DropdownMenuItem>
               <DropdownMenuItem>Team</DropdownMenuItem>
               <DropdownMenuItem>Subscription</DropdownMenuItem>
+            </DropdownMenuContent>
+          </DropdownMenu>
+          <DropdownMenu>
+            <DropdownMenuTrigger asChild>
+              <Button variant="outline" size="icon">
+                <Sun className="h-[1.2rem] w-[1.2rem] rotate-0 scale-100 transition-all dark:-rotate-90 dark:scale-0" />
+                <Moon className="absolute h-[1.2rem] w-[1.2rem] rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100" />
+                <span className="sr-only">Toggle theme</span>
+              </Button>
+            </DropdownMenuTrigger>
+            <DropdownMenuContent align="end">
+              <DropdownMenuItem onClick={() => setTheme("light")}>
+                Light
+              </DropdownMenuItem>
+              <DropdownMenuItem onClick={() => setTheme("dark")}>
+                Dark
+              </DropdownMenuItem>
+              <DropdownMenuItem onClick={() => setTheme("system")}>
+                System
+              </DropdownMenuItem>
             </DropdownMenuContent>
           </DropdownMenu>
         </div>
