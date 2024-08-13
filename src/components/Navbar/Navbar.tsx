9e4f79a761df1732d8d9665e5e790f5779464b63
P A Chathura Madhava <chathuratechdev@gmail.com>
2024-08-13 11:57:01 +0530

    Update Navbar Component

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -1,18 +1,76 @@
-import Image from "next/image"
+import Image from "next/image";
 import companyLogo from "@/assets/company-logo.png";
+import {
+  NavigationMenu,
+  NavigationMenuContent,
+  NavigationMenuItem,
+  NavigationMenuLink,
+  NavigationMenuList,
+  NavigationMenuTrigger,
+  navigationMenuTriggerStyle,
+} from "../ui/navigation-menu";
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuLabel,
+  DropdownMenuSeparator,
+  DropdownMenuTrigger,
+} from "../ui/dropdown-menu";
+import Link from "next/link";
 
 function Navbar() {
   return (
-    <div className="flex items-center py-2">
-      <div className="flex items-center">
-      <Image src={companyLogo} alt="company-logo" height={60} width={60} />
-      <h2 className="text-2xl font-bold">MODERN WALK</h2>
+    <div>
+      <div className="flex items-center py-2 justify-between pr-4">
+        <Link className="flex items-center" href={"/"}>
+          <Image src={companyLogo} alt="company-logo" height={60} width={60} />
+          <h2 className="text-2xl font-bold">MODERN WALK</h2>
+        </Link>
+
+        <div>
+          <NavigationMenu>
+            <NavigationMenuList>
+              <NavigationMenuItem>
+                <Link href={"/"} legacyBehavior passHref>
+                  <NavigationMenuLink className={navigationMenuTriggerStyle()}>
+                    Home
+                  </NavigationMenuLink>
+                </Link>
+              </NavigationMenuItem>
+              <NavigationMenuItem>
+                <NavigationMenuTrigger>Men's Clothing</NavigationMenuTrigger>
+                <NavigationMenuContent>
+                  <NavigationMenuLink>Link</NavigationMenuLink>
+                </NavigationMenuContent>
+              </NavigationMenuItem>
+              <NavigationMenuItem>
+                <NavigationMenuTrigger>Women's Clothing</NavigationMenuTrigger>
+                <NavigationMenuContent>
+                  <NavigationMenuLink>Link</NavigationMenuLink>
+                </NavigationMenuContent>
+              </NavigationMenuItem>
+            </NavigationMenuList>
+          </NavigationMenu>
+        </div>
+        <div className="flex gap-4">
+          <div>Cart</div>
+          <DropdownMenu>
+            <DropdownMenuTrigger>Open</DropdownMenuTrigger>
+            <DropdownMenuContent>
+              <DropdownMenuLabel>My Account</DropdownMenuLabel>
+              <DropdownMenuSeparator />
+              <DropdownMenuItem>Profile</DropdownMenuItem>
+              <DropdownMenuItem>Billing</DropdownMenuItem>
+              <DropdownMenuItem>Team</DropdownMenuItem>
+              <DropdownMenuItem>Subscription</DropdownMenuItem>
+            </DropdownMenuContent>
+          </DropdownMenu>
+        </div>
       </div>
-      <div>
-      <button>Home</button>
-      </div>
+      <div className="bg-slate-200 h-[4px]"></div>
     </div>
-  )
+  );
 }
 
-export default Navbar
+export default Navbar;
--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -4,8 +4,8 @@ import ProductCard from "@/components/ProductCard/ProductCard";
 function HomePage() {
   return (
     <div>
-      <h1 className="text-4xl font-bold text-center mt-2">Moden Walk</h1>
-      <div className="bg-slate-200 h-[4px] mt-2"></div>
+      {/* <h1 className="text-4xl font-bold text-center mt-2">Moden Walk</h1> */}
+      
       <div className="container mx-auto mt-4 px-44">
         <h3 className="font-bold">Flash Sale</h3>
         <div className="flex justify-around items-center py-6">
